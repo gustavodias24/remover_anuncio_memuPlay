99689654dbc57363d620fad838a07bb3a78f24cd
gusta <gustavo024dias@gmail.com>
2023-11-06 13:40:23 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from pandasmemuc import MeMuc
+alldevices = MeMuc()
+
+# alldevices.patch_hosts(vms_aa_index=alldevices.aa_index.to_list(), timeout=120)
+# alldevices.bb_reboot()
+# alldevices.bb_reboot.iloc[0] or alldevices.bb_reboot.apply(lambda x:x())
